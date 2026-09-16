9f0b701cb72d120124fca9c05ae57e2526def7b9
stevenegoavil <steven.vidal.egoavil@gmail.com>
2026-09-16 10:59:20 -0400

    saving

---
+++ ethernaut/Phase1/lesson2/constructor_testcode/Charity/charity2.sol
@@ -0,0 +1,12 @@
+//second attempt
+//SPDX-License-Identifier:MIT;
+pragma solidity >=0.4.0<0.9.0;
+
+contract charity2 {
+    address public owner;
+
+    constructor(uint256, _goalAmount) {
+        owner = msg.sender;
+        goalAmount = _goalAmount;
+    }
+}
